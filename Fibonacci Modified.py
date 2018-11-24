5baeaa8fda743dbd059b2c304330032e836053c9
Shuangquan Feng <fengsq@utexas.edu>
2018-11-24 09:32:27 -0600

    py unlimited int

---
+++ Fibonacci Modified.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the fibonacciModified function below.
+def fibonacciModified(t1, t2, n):
+    a, b = t1, t2;
+    for i in range(3, n+1):
+        t = a + b*b;
+        a = b;
+        b = t;
+    
+    return b;
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t1T2n = input().split()
+
+    t1 = int(t1T2n[0])
+
+    t2 = int(t1T2n[1])
+
+    n = int(t1T2n[2])
+
+    result = fibonacciModified(t1, t2, n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
