36a3d1cc71ec9573f1bd84d22ac3e769808eba9c
Shuangquan Feng <fengsq@utexas.edu>
2018-12-27 21:04:04 +0800

    easy

--- Drawing Book.cpp
+++ Drawing Book.cpp
@@ -3,6 +3,6 @@
 int main() {
     int n, p;
     cin >> n >> p;
-    cout << min(p>>1, (n&1) ? ((n+1-p+1)/2-1) : (n-p+1)/2) << endl;
+    cout << min(p>>1, (n&1) ? (n-p)/2 : (n-p+1)/2) << endl;
     return 0;
 }
