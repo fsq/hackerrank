e3431cea07fc325a5c325b8fb547881ec4ae3727
Shuangquan Feng <fengsq@utexas.edu>
2018-12-29 14:42:13 +0800

    ptr

---
+++ Goodland Electricity.cpp
@@ -0,0 +1,33 @@
+#include "stdc++.h"
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+    VI a(n);
+    FOR(i, n) cin >> a[i];
+
+    int ans = 0, pre = INT_MIN;
+    for (int nxt, j,i=0; i<n; ) {
+        nxt = -1;
+        for (j=i; j<n && j<i+k; ++j) 
+            if (a[j]==1)
+                nxt = j;
+        if (nxt==-1) {
+            if (pre+k > i) {
+                ++ans;
+                i = pre + k;
+            } else {
+                ans = -1;
+                break;
+            }
+        } else {
+            i = nxt + k;
+            ++ans;
+            for (; nxt<i; ++nxt)
+                if (a[nxt])
+                    pre = nxt;
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
